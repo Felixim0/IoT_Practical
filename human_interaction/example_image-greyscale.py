385e010d0f3b7ee5086e6ef632956373109c75fe
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-02-13 16:39:45 +0000

    Added greyscale and also edge image highlihgting examples. Similar to https://github.com/portsoc/websight

--- human_interaction/example_image-greyscale.py
+++ human_interaction/example_image-greyscale.py
@@ -8,15 +8,7 @@ while camera.isOpened():
   cv2.imshow('Gray image', gray)
   ret, blackwhite = cv2.threshold(gray, 40, 110, cv2.THRESH_BINARY_INV)
   cv2.imshow('Binary image', blackwhite)
-
-  edges = cv2.Canny(gray, 50, 150)
-  cv2.imshow('Edges', edges)
-  corners = cv2.cornerHarris(gray, 3, 5, 0.06)
-  # corners = cv2.dilate(corners, None)
-  cornerMarkers = frame
-  cornerMarkers[corners > 0.01 * corners.max()] = [0, 0, 255]
-  cv2.imshow('Corners', cornerMarkers)
-    
+  
   if cv2.waitKey(10) == 27:
     break
 
