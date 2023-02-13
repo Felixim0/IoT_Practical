77aa9474904ae5361eb6dedc4974d816cb38ca17
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2023-02-13 17:33:12 +0000

    Added voice recording example

---
+++ human_interaction/audio-interactions/test.py
@@ -0,0 +1,13 @@
+import time
+import speech_recognition as sr
+time_now = time.strftime("%Y%m%d-%H%M%S")
+r = sr.Recognizer() # turn on your microphone
+mic = sr.Microphone()
+with mic as source:
+  r.adjust_for_ambient_noise(source) # denoise
+  audio = r.record(source, 2) # record 2 seconds from the microphone
+  # or you can use r.listen(source) to keep listening to microphone until timeout (no voice)
+  with open(f"Voice-{time_now}.wav", "wb") as f:
+    # save your voice in wav for further processing
+    f.write(audio.get_wav_data(convert_rate=16000))
+    print('Voice Saved')
